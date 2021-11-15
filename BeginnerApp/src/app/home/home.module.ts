633f2c51dbf5453eaea25edcb6d8d7bafeb11cee
dawid <d.krainski@cez.gov.pl>
2021-11-15 01:39:25 +0100

    new project generate

---
+++ BeginnerApp/src/app/home/home.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LanguagesListComponent } from './languages-list/languages-list.component';
+
+
+
+@NgModule({
+  declarations: [
+    LanguagesListComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class HomeModule { }
